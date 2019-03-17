75ec08f40caff87e36d2c00a4cc465e4894861ba
Giorgio Delgado <hi@gdelgado.ca>
2019-03-17 18:51:43 -0400

    rm irrelevant comments

--- src/index.js
+++ src/index.js
@@ -6,7 +6,4 @@ require('./css/custom.css')
 
 var Elm = require('./elm/Main.elm').Elm;
 
-// .embed() can take an optional second argument.
-// This would be an object describing the data we need to start a program
-// i.e. a userID or some token
 var app = Elm.Main.init();
