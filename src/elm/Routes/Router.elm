2fd70bb8b45da297b793efd0d066ed1c1927a853
Giorgio Delgado <hi@gdelgado.ca>
2019-03-18 21:54:42 -0400

    Begin working on simpler router

--- src/elm/Routes/Router.elm
+++ src/elm/Routes/Router.elm
@@ -1,11 +1,19 @@
 module Routes.Router exposing
   ( Details
   , view
+  , fromUrl
+  , Route(..)
   )
 
 
 import Html as Html exposing (..)
 import Browser
+import Url exposing (Url)
+import Url.Parser as Parser exposing (Parser, oneOf, s, string)
+
+type Route
+  = Home
+  | Admin
 
 
 type alias Details msg =
@@ -13,6 +21,17 @@ type alias Details msg =
   , header : Html msg
   , children : List (Html msg)
   }
+
+
+parser : Parser (Route -> a) a
+parser =
+  oneOf
+    [ Parser.map Home Parser.top
+    , Parser.map Admin (s "admin")
+    ]
+
+fromUrl : Url -> Maybe Route
+fromUrl = Parser.parse parser
 
 
 view : (a -> msg) -> Details a -> Browser.Document msg
