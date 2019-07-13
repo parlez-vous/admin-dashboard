67ac11de2733fb869f7172bb3cd56f72bf83c241
Giorgio Delgado <hi@gdelgado.ca>
2019-07-13 10:17:05 -0700

    Add loading component

---
+++ src/elm/UI/Loader.elm
@@ -0,0 +1,10 @@
+-- Exposes a loding UI component
+module UI.Loader exposing (donut)
+
+import Html exposing (Html, div)
+import Html.Attributes exposing (class)
+
+donut : Html msg
+donut =
+  div [ class "loading__donut" ] []
+
