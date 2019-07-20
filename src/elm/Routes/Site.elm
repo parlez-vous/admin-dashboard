22365b7fe9fd1292da2dbe6de6f1f65f1f53ee8b
Giorgio Delgado <hi@gdelgado.ca>
2019-07-19 23:42:55 -0400

    Add scaffolding for site route

--- src/elm/Routes/Site.elm
+++ src/elm/Routes/Site.elm
@@ -1,9 +1,50 @@
 -- TODO
 
-module Site exposing ( id )
+module Site exposing
+  ( Model
+  , init
+  )
+
+
+import Browser.Navigation as Nav
+import SharedState exposing (SharedState)
+
+import Html exposing (..)
+
+
+type alias Model =
+  { siteId : Int
+  }
 
 
 
-id : a -> a
-id a = a
+-- initialize the model
+init : Int -> Model
+init = Model
 
+
+-- Get site from SharedState
+-- Or issue HTTP req to fetch
+-- this specific site
+initRoute :
+  String ->
+  String ->
+  Nav.Key ->
+  Cmd msg
+initRoute token api navKey = Cmd.none
+
+
+
+{--
+{ navKey  = key
+  , api     = api
+  , session = RemoteData.NotAsked
+--}
+
+type alias Title = String
+
+view : SharedState -> Model -> (Title, Html msg)
+view state model =
+  ( "Site"
+  , div [ ] [ text "" ]
+  )
