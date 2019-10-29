d2791574c7376520cec5b70d6129e8e7fd056794
Giorgio Delgado <hi@gdelgado.ca>
2019-10-29 00:13:31 -0400

    Add logo to navbar

--- src/elm/UI/Hnav.elm
+++ src/elm/UI/Hnav.elm
@@ -4,11 +4,15 @@ Horizontal Nav
 
 module UI.Hnav exposing (hnav)
 
-import Html exposing (Html, header, nav)
-import Html.Attributes as Attributes exposing (class, classList)
-import Html.Events exposing (onClick)
+import Html exposing (Html, div, header, nav)
+import Html.Attributes as Attributes exposing (class)
+import UI.Icons exposing (logo)
 
 hnav : List (Html msg) -> Html msg
 hnav children =
-  header [ class "w-auto h-16 pt-4 pb-2 md:px-4 mt-3 flex justify-end" ]
-    [ nav [] children ]
+  header [ class "w-auto h-20 pt-6 pb-2 md:px-4 flex justify-between" ]
+    [ div []
+        [ logo "45"
+        ]
+    , nav [] children
+    ]
