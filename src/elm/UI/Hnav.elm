deb8296b2e3026cfcb0a1a5a438926d7477c2238
Giorgio Delgado <hi@gdelgado.ca>
2019-10-28 21:23:01 -0400

    Fix markup and css of hnav

--- src/elm/UI/Hnav.elm
+++ src/elm/UI/Hnav.elm
@@ -4,11 +4,11 @@ Horizontal Nav
 
 module UI.Hnav exposing (hnav)
 
-import Html exposing (Html, nav, div)
+import Html exposing (Html, header, nav)
 import Html.Attributes as Attributes exposing (class, classList)
 import Html.Events exposing (onClick)
 
 hnav : List (Html msg) -> Html msg
 hnav children =
-  nav [ class "pt-4 pb-2 overflow-auto" ]
-    [ div [ class "float-right" ] children ]
+  header [ class "w-auto h-16 pt-4 pb-2 md:px-4 mt-3 flex justify-end" ]
+    [ nav [] children ]
