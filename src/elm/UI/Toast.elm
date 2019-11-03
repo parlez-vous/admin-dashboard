2d730e569341fad36d86a6cf623fd3ec3ddabad8
Giorgio Delgado <hi@gdelgado.ca>
2019-11-03 13:47:22 -0500

    Use tailwind css for toast

--- src/elm/UI/Toast.elm
+++ src/elm/UI/Toast.elm
@@ -6,8 +6,10 @@ module UI.Toast exposing
   )
 
 import Html exposing (Html, div, text)
-import Html.Attributes exposing (class)
+import Html.Attributes exposing (class, classList)
 import Toasty exposing (Stack)
+
+import Utils
 
 
 type alias ToastState = Stack String
@@ -21,12 +23,26 @@ config : Toasty.Config msg
 config = Toasty.config
 
 
+-- TODO: customize inset to provide some space
+-- between the bottom and the toast on wider
 view : (Toasty.Msg String -> msg) -> ToastState -> Html msg
 view toMsg toastStack =
-  div [ class "toast__container" ]
+  div [ class "fixed inset-x-0 bottom-0" ]
     [ Toasty.view config renderToast toMsg toastStack ]
     
 
 renderToast : String -> Html msg
 renderToast toast =
-  div [ class "toast__item" ] [ text toast ]
+  let
+    classes = Utils.toClass
+      [ "py-1"
+      , "px-2"
+      , "bg-red-200"
+      , "text-red-700"
+      , "rounded"
+      , "mb-4"
+      , "md:mx-auto"
+      , "md:w-1/6"
+      ]
+  in
+  div [ classes ] [ text toast ]
