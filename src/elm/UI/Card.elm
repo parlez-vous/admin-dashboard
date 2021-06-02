f7072f5a1b4182742a3ecbd6cf6aaa9dbe99ccef
asrar <aszenz@gmail.com>
2021-06-03 01:11:29 +0530

    initial card element

--- src/elm/UI/Card.elm
+++ src/elm/UI/Card.elm
@@ -6,4 +6,4 @@ import Html.Attributes exposing (class)
 
 card : List (Html msg) -> Html msg
 card body =
-    div [class "rounded"] body
+    div [ class "max-w-md py-4 px-8 bg-white shadow-lg rounded-lg my-10 mt-2 text-gray-600" ] body
